7485bb5142069a19bac41c056789179a04a65132
Hareendran M G <hareendranmg@gmail.com>
2023-01-02 15:05:07 +0000

    FEAT: Another server code added

--- ws_server_3.py
+++ ws_server_3.py
@@ -6,20 +6,19 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)
 
 
 async def handler(websocket, path):
-    async for req in websocket:
-        if req == 'connect':
-            print("Connected...")
-            await websocket.send("Connected")                    
-        if req == 'gps':
-            while req != 'disconnect':
+    async for req in websocket:                   
+        if req == 'gps_on':
+            while req != 'gps_off':
                 data = ser.readline()
                 # time.sleep(0.5)
                 print("Sending: " + data.decode('utf-8'))
-                await websocket.send(data)                    
-        if req == 'disconnect':
+                await websocket.send(data)   
+                # req = await websocket.recv()
+        if req == 'gps_off':
             s.close()
             print('Disconnected...')
-            await websocket.send("Disconnected")                    
+            await websocket.send("Disconnected") 
+                           
 
 
 start_server = websockets.serve(handler, "192.168.2.115", 8765)
